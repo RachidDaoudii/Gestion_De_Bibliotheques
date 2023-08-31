ffa9ce5290ff992e14a51a853f78ec7f24bc3d82
RachidDaoudii <rachiddaoudi533@gmail.com>
2023-08-31 15:45:49 +0200

    creation class Livres et connexion base de donner avec projet

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
+import _class.Livres;
+import db.DatabaseConnection;
+import _class.Menu;
+
+import java.util.*;
 public class Main {
     public static void main(String[] args) {
-        
+        //DatabaseConnection dbConnection = new DatabaseConnection();
+
+        //Livres livres = new Livres();
+
+        //livres.retrieveUsers();
+
+        //dbConnection.closeConnection();
+        Scanner in = new Scanner(System.in);
+        Menu menu = new Menu();
+        menu.ListMenu();
+        //String choix = in.nextLine();
+        //menu.direBonjourAvecSwitch(choix);
+
     }
 }
